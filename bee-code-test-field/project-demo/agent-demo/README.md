c8f4935f4a8193072aa0833a9f7c2c1fce3646b1
Cyuxuan <cyuxuan@163.com>
2023-03-29 23:54:18 +0800

    提交获取当前线程的内存使用量

--- bee-code-test-field/project-demo/agent-demo/README.md
+++ bee-code-test-field/project-demo/agent-demo/README.md
@@ -1,3 +1,4 @@
 
 # 在目标项目VM参数中指定对应的agent
 -javaagent:D:\c_root\learn\learn\learn-java-source\bee-code-test-field\project-demo\agent-demo\target\agent-demo-1.0-SNAPSHOT.jar=cn.cyuxuan
+-javaagent:D:/c_root/learn/learn/learn-java-source/bee-code-test-field/project-demo/agent-demo/target/agent-demo-1.0-SNAPSHOT.jar=cn.cyuxuan.agent
--- bee-code-test-field/project-demo/spring-web-test-field/src/main/java/cn/cyuxuan/util/PerfAspect.java
+++ bee-code-test-field/project-demo/spring-web-test-field/src/main/java/cn/cyuxuan/util/PerfAspect.java
@@ -1,5 +1,6 @@
 package cn.cyuxuan.util;
 
+import com.sun.management.ThreadMXBean;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.After;
@@ -10,7 +11,10 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
 import java.util.*;
+import java.util.function.BiConsumer;
 
 /**
  * 函数执行时间分析
@@ -18,7 +22,7 @@ import java.util.*;
  * @author 陈玉轩
  * @since 23.03.26
  */
-//@Aspect
+@Aspect
 @Component
 public class PerfAspect {
     private final static String PREFIX = "方法 ";
@@ -42,6 +46,33 @@ public class PerfAspect {
         Signature signature = joinPoint.getSignature();
         MethodSignature methodSignature = (MethodSignature) signature;
         String methodName = methodSignature.getMethod().getName();
+        ExecuteInfo executeInfo = new ExecuteInfo();
+        // 通过 method name 生成一组执行数据并记录下来
+        this.handleBeforeData(methodName, executeInfo);
+        // 获取此线程当前函数执行前的字节数量,以及前函数执行前的 gc 次数
+        this.getThreadAllocatedBytes((allocatedBytes, gcNums) -> {
+            executeInfo.beforeByteNum = allocatedBytes;
+            executeInfo.beforegGcNum = gcNums;
+        });
+    }
+
+    /**
+     * 后置执行函数
+     *
+     * @param joinPoint
+     */
+    @After(value = "pointCut()")
+    public void after(JoinPoint joinPoint) {
+        // 处理方法执行后的数据
+        this.handleAfterData();
+    }
+
+    /**
+     * 处理方法执行前的数据处理
+     *
+     * @param methodName 方法名称
+     */
+    private void handleBeforeData(String methodName, ExecuteInfo executeInfo) {
         Map<String, Object> localData = new HashMap<>();
         localData = threadLocal.get();
         // 执行栈
@@ -69,7 +100,6 @@ public class PerfAspect {
         // 使用uuid进行时间标记
         StopWatch stopWatch = new StopWatch(uuid);
         // 存储执行信息
-        ExecuteInfo executeInfo = new ExecuteInfo();
         executeInfo.executeId = uuid;
         executeInfo.methodName = methodName;
         executeInfo.level = stack.size();
@@ -83,12 +113,9 @@ public class PerfAspect {
     }
 
     /**
-     * 后置执行函数
-     *
-     * @param joinPoint
+     * 方法执行后的书处理
      */
-    @After(value = "pointCut()")
-    public void after(JoinPoint joinPoint) {
+    private void handleAfterData() {
         // 方法执行结束，取出stopWatch
         Stack<StopWatch> stack = (Stack<StopWatch>) threadLocal.get().get("stack");
         StopWatch stopWatch = stack.pop();
@@ -116,6 +143,25 @@ public class PerfAspect {
     }
 
     /**
+     * 获取当前线程已经申请到的字节数
+     *
+     * @return 当前线程已经申请到的字节数
+     */
+    private void getThreadAllocatedBytes(BiConsumer<Long, Long> function) {
+        com.sun.management.ThreadMXBean threadMXBean
+                = (ThreadMXBean) ManagementFactory.getThreadMXBean();
+        long threadId = Thread.currentThread().getId();
+        long allocatedBytes = threadMXBean.getThreadAllocatedBytes(threadId);
+        long gcNum = 0;
+        for (final GarbageCollectorMXBean garbageCollector
+                : ManagementFactory.getGarbageCollectorMXBeans()) {
+            gcNum += garbageCollector.getCollectionCount();
+        }
+        function.accept(allocatedBytes, gcNum);
+    }
+
+
+    /**
      * 执行信息
      */
     private class ExecuteInfo {
@@ -138,5 +184,25 @@ public class PerfAspect {
          * 当前函数执行的时间
          */
         public long executeTime;
+
+        /**
+         * 当前线程方法执行前的字节数量
+         */
+        public long beforeByteNum;
+
+        /**
+         * 当前线程方法执行后的字节数量
+         */
+        public long afterByteNum;
+
+        /**
+         * 方法执行前的gc次数
+         */
+        public long beforegGcNum;
+
+        /**
+         * 方法执行后的gc次数
+         */
+        public long afterGcNum;
     }
 }
